9664d21f48ed6172536b89e3447726742e84db59
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-04-09 20:31:07 +0600

    constructor initalizer done

--- 5-constructor-initalizer.cpp
+++ 5-constructor-initalizer.cpp
@@ -5,9 +5,19 @@ class Student
 {
 public:
     const int age;
+    Student(int x) : age(x)
+    {
+        cout << "Your Age is : " << age << endl;
+    }
+    void display()
+    {
+        cout << "I am from Display Function!!!" << endl;
+    }
 };
 
 int main()
 {
+    Student a(23);
+    a.display();
     return 0;
 }
