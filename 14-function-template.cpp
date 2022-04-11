7b2656018bbe6ced5731a51048e60336ed5e76f5
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-04-11 11:53:17 +0600

    function template has been created

---
+++ 14-function-template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <class myTemplate>
+myTemplate add(myTemplate a, myTemplate b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << "Addition of int number is : " << add(5, 26) << endl;
+    cout << "Addition of Float number is : " << add(5.5559, 26.555) << endl;
+
+    return 0;
+}
